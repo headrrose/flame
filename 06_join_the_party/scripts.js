19916fae87995c3eef7ab14dfa50a4df0f524400
Mur11veMytt0n <headr.rose@gmail.com>
2026-06-15 16:59:12 +1200

    Part 5 finshed

--- 06_join_the_party/scripts.js
+++ 06_join_the_party/scripts.js
@@ -1,10 +1,13 @@
 let nameInput = document.getElementById("nameInput");
 let greetButton = document.getElementById("greetButton");
 let output = document.getElementById("output");
+let roleInput = document.getElementById("roleInput")
+let partyInput = document.getElementById("partyInput")
 
 function sayHello() {
   let name = nameInput.value;
-  output.innerText = "Welcome, " + name;
+  let role = roleInput.value;
+  let party = partyInput.value;
+  output.innerText = "Welcome, " + name + " the " + role + " to the " + party.toUpperCase()
 }
-
 greetButton.onclick = sayHello;
