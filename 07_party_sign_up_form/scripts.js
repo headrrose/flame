8dc3395be1f1ccd74d7ce806dd29fc129a84227b
Mur11veMytt0n <headr.rose@gmail.com>
2026-06-17 17:51:21 +1200

    Part 7 or sm

--- 07_party_sign_up_form/scripts.js
+++ 07_party_sign_up_form/scripts.js
@@ -1,9 +1,22 @@
 function showSignupMessage() {
   let name = document.getElementById("nameInput").value;
+
   let region = document.getElementById("regionInput").value;
 
-  let message = `Welcome, ${name} from ${region} Thanks for joining the party`
+  let role = document.getElementById("roleInput").value;
+
+  let message
+  if (name == ''){
+    message = "please enter both name and"
+
+  } else if (region == "") {
+    message = 'please enter valid'
+  } else {
+  message = `Welcome, ${name} the ${role} from ${region} \nThanks for joining the party`
+  }
+
   document.getElementById("output").innerText = message;
+   
 }
 
 document.getElementById("signupButton").onclick = showSignupMessage
