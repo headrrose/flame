0bbefa4fbc972451bdd336e632685e28a4b5c8e2
Mur11veMytt0n <headr.rose@gmail.com>
2026-06-23 17:02:52 +1200

    AHHH

--- 09_can_you_vote/scripts.js
+++ 09_can_you_vote/scripts.js
@@ -1,17 +1,22 @@
 // Instructions in the CSS
+function checkAge() {
 
-fuction checkAge = 
-  let age = document.getElement("ageBox").value;
-
-  when age >= 18 {
+  let age = Number(document.getElementById("ageBox").value);
+  
+  if (age >= 99) {
+    message = "You're a legend!?"
+  } else if (age >= 18) {
     message = "You can vote!";
-  } else if age >= 16 {
+  } else if (age >= 16) {
     message = "You can pre-enrol.";
+  } else if (age <= 0) {
+    message = "Time traveller??"
   } else {
-    mesage = "Sorry, you can't register yet.";
+    message = "Sorry, you can't register yet.";
   }
 
-  doccument.getelementbyid("#results").internalhtml = message;
 
+  document.getElementById("result").innerHTML = message;
+}
 
-document.getElementID("submit").onClick = checkAge
+document.getElementById("submit").onclick = checkAge
