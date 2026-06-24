e97d721e986749f0b1861a939fffce8bbaee2d8f
Mur11veMytt0n <headr.rose@gmail.com>
2026-06-24 20:02:44 +1200

    Energy ships idk

--- 12_flamejack/scripts.js
+++ 12_flamejack/scripts.js
@@ -1,15 +1,19 @@
 let energy = 0;
 
 function chargeReactor() {
-  let pulse = Math.floor(Math.random() * 10) + 1; // 1-10
+  let pulse = Math.floor(Math.random() * 5) + 1; // 1-10
   energy += pulse;
 
-  document.getElementById("energyDisplay").innerText = "Energy: " + energy + " / 100";
+  document.getElementById("energyDisplay").innerText = "Energy: " + energy + " / 21";
 
   if (energy < 16) {
     document.getElementById("statusMessage").innerText = " Charging...";
-  } else if (energy <= 21) {
+  } else if (energy < 21) {
     document.getElementById("statusMessage").innerText = " Stable energy level!";
+  } else if (energy == 21) {
+    document.getElementById("statusMessage").innerText = " Perfect Resonance!";
+  } else if (energy <= 23) {
+    document.getElementById("statusMessage").innerText = " DANGER: Energy level rising..!";
   } else {
     document.getElementById("statusMessage").innerText = " Overload! Core meltdown!";
     document.getElementById("chargeButton").disabled = true;
