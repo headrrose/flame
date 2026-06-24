7cc4a5011d2a34fe9d0d34d7afc3fdffab699048
Mur11veMytt0n <headr.rose@gmail.com>
2026-06-24 13:51:27 +1200

    Hah easy

--- 10_find_your_party_role/scripts.js
+++ 10_find_your_party_role/scripts.js
@@ -1,12 +1,16 @@
 function matchRole() {
   let positionElement = document.querySelector('input[name="alignment"]:checked');
+  let houseElement = document.querySelector('input[name="house"]:checked')
   let message = "";
 
   if (!positionElement) {
     message = "Please choose a party alignment.";
+  } else if (!houseElement) {
+    message = "Please select your house.";
   } else {
     let position = positionElement.value;
     let selectedIssues = [];
+    let house = houseElement.value;
 
     let issueBoxes = document.querySelectorAll('.issue');
     for (let i = 0; i < issueBoxes.length; i++) {
@@ -17,13 +21,17 @@ function matchRole() {
 
     if (selectedIssues.length == 0) {
       message = "Please select at least one issue you care about.";
+    } else if (selectedIssues.length == 3 || selectedIssues.length == 4){
+      message = "Too many issues chosen"
+    } else if (house == "yellow" && selectedIssues.includes("economy")) {
+      message = "👑 You're a a royal";
     } else if (position == "progressive" && selectedIssues.includes("environment")) {
       message = "🌱 You’d be perfect for the Outreach Team.";
     } else if (position == "moderate" && selectedIssues.includes("economy")) {
       message = "📊 You’d make a great Policy Analyst."
     } else if (position == "traditional" || selectedIssues.includes("education")) {
       message = "🎖️ You’d bring valuable perspective to Strategy.";
-    } else {
+    }  else {
       message = "🤝 You’d be a great all-round supporter!";
     }
   }
